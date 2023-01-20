6db569ec9cbc5d3d435cd637ed047996e14323dd
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-20 09:19:15 +0900

    모의고사 => 반복되는것을 미리 배열로 정해놓는다.

---
+++ 2023/January/프로그래머스/모의고사.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    result = []
+
+    one = [1, 2, 3, 4, 5]
+    two = [2, 1, 2, 3, 2, 4, 2, 5]
+    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    score = [0, 0, 0]
+
+    for i, answer in enumerate(answers):
+        if one[i%5] == answer:
+            score[0] += 1
+        if two[i%8] == answer:
+            score[1] += 1
+        if three[i%10] == answer:
+            score[2] += 1
+
+    for i in range(len(score)):
+        if max(score) == score[i]:
+            result.append(i+1)
+
+    return result
