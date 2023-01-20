321209a7f97ffda19888f980d0b639c941705bc4
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-21 08:53:24 +0900

    JadanCase 문자열 만들기 => 쉽게 생각하면 된다. 어렵게 생각하면 오히려 안풀림.

---
+++ 2023/January/프로그래머스/JadenCase 문자열 만들기 .py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    x = s.lower().split(' ')
+
+    for i in range(len(x)):
+        if x[i] != '':
+            x[i] = x[i][0].upper() + x[i][1:]
+
+    print(x)
+    print(answer)
+    answer = ' '.join(x)
+
+    return answer
