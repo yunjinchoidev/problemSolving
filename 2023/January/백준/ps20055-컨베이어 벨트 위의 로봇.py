7996b46f3a326e2cb3a10ff9d8dd75731ba2bdd5
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-20 21:14:01 +0900

    컨베이어 벨트 위의 로봇 => deque.rotate() 함수 사용, 구현, 시뮬레이션 문제

---
+++ 2023/January/백준/ps20055-컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+N, K = map(int, input().split())
+numbers = deque(list(map(int, input().split())))
+
+lv = 1
+robot = deque([False for i in range(N)])
+while True:
+    # 돌리기
+    numbers.rotate()
+    robot.rotate()
+
+    robot[N-1] = False
+
+
+    for i in range(N - 2, -1, -1):
+        if robot[i] == True \
+                and robot[i + 1] == False \
+                and numbers[i + 1] > 0:
+            robot[i] = False
+            robot[i + 1] = True
+            numbers[i + 1] -= 1
+
+    robot[N - 1] = False
+    if numbers[0] > 0:
+        robot[0] = True
+        numbers[0] -= 1
+    if numbers.count(0) >= K:
+        break
+
+    lv += 1
+
+print(lv)
