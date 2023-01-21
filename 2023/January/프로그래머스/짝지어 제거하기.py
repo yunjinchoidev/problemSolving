4c0146ff6e3efc29a46ac14b4b1606230b761b4a
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-21 10:51:39 +0900

    짝지어 제거하기 => # 스택을 이용해 간단하게 풀 수 있다.. .

---
+++ 2023/January/프로그래머스/짝지어 제거하기.py
@@ -0,0 +1,25 @@
+def solution(s):
+    answer = -1
+
+    s_list = list(s)
+    stack = []
+
+    for i in s_list:
+        if not stack:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+
+    if len(stack) >= 1:
+        answer = 0
+    else:
+        answer = 1
+
+    return answer
+
+
+
+# 스택을 이용해 간단하게 풀 수 있다.. .
