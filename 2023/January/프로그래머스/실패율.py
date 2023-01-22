0dbdbc806dba2b3bed4a6e5ea32c8bf4edee9412
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-22 17:32:40 +0900

    실패율 => 구현

---
+++ 2023/January/프로그래머스/실패율.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+def solution(N, stages):
+    answer = []
+    x = Counter(stages)
+    stages.sort()
+    s = set(stages)
+
+    fail = [[i, 0] for i in range(N + 2)]
+    visited = [False for i in range(N + 2)]
+    for i in range(len(stages)):
+        if not visited[stages[i]]:
+            visited[stages[i]] = True
+            fail[stages[i]][1] = x[stages[i]] / (len(stages) - i)
+    del fail[0]
+    del fail[len(fail) - 1]
+
+    qq = sorted(fail, key=lambda x: (-x[1], x[0]))
+    print(qq)
+    for i in qq:
+        answer.append(i[0])
+
+    return answer
