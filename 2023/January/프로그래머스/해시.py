e863283aeb949805585aa8c37c47eacbfc5108b5
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-22 10:27:37 +0900

    해시 => dict 순회에 기억하자

---
+++ 2023/January/프로그래머스/해시.py
@@ -0,0 +1,17 @@
+def solution(clothes):
+    answer = 1
+    dict = {}
+
+    for i in range(len(clothes)):
+        # print(clothes[i][0],clothes[i][1])
+        if clothes[i][1] not in dict:
+            dict[clothes[i][1]] = 1
+        else:
+            dict[clothes[i][1]] += 1
+    # print(dict)
+
+    for idx, value in dict.items():
+        # print(idx, value)
+        answer *= (value + 1)
+
+    return answer - 1
