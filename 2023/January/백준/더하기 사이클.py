66320837d2746f1b75f6cd6f244c930e37adc383
yunjinchodev <yunjinchoidev@gmail.com>
2023-01-24 17:40:39 +0900

    백준 더하기 사이클 => 백준

---
+++ 2023/January/백준/더하기 사이클.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+origin = N
+cnt = 0
+while True:
+
+    if N < 10:
+        numbers = [0, N]
+    else:
+        numbers = list(map(int, str(N)))
+
+    N = sum(numbers)
+    N_list = list(map(int, str(N)))
+    N = int(numbers[-1])*10 + int(N_list[-1])
+    cnt += 1
+
+    if origin == N :
+        break
+print(cnt)
