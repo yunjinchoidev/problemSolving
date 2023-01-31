836766db4750cb40385babefbbafc45b4334b164
yunjinchoidev <yunjinchoidev@gmail.com>
2023-01-31 21:17:20 +0900

    2545. Sort the Students by Their Kth Score => 이차원 배열 정렬, enumerate 함수 사용

---
+++ 2023/January/리트코드/2545. Sort the Students by Their Kth Score.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        r = []
+        answer = []
+        for idx, value in enumerate(score):
+            r.append((value[k], idx))
+
+        r.sort(key = lambda x:-x[0])
+        # print(r)
+
+        for idx, value in enumerate(r):
+            answer.append(score[value[1]])
+
+        return answer
