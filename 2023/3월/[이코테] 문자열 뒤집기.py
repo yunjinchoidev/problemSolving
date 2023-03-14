42117db24c701510047411a193e8f6346035ddb0
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-14 21:56:17 +0900

    [이코테] 문자열 뒤집기

---
+++ 2023/3월/[이코테] 문자열 뒤집기.py
@@ -0,0 +1,14 @@
+N = input()
+strings = list(map(str, N))
+
+answer = 0
+
+for i in range(len(strings)-1):
+
+    if strings[i] != strings[i + 1]:
+        answer += 1
+
+if answer % 2 == 0:
+    print(int(answer/2))
+else:
+    print(int(answer//2)+1)
