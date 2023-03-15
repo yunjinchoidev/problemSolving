2b8ea45f419bb4886036ee2a0296ddf98a82138d
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-15 09:03:25 +0900

    [이코테] 만들 수 없는 금액

--- 2023/3월/[이코테] 만들 수 없는 금액.py
+++ 2023/3월/[이코테] 만들 수 없는 금액.py
@@ -1,26 +1,36 @@
 N = int(input())
 numbers = list(map(int, input().split()))
-visited = [False for i in range(1000000)]
 
+# 최솟값부터 확인한다.
+numbers.sort()
 
-def DFS(n):
-    stack = []
-    stack.append(n)
-    visited[n] = True
+idx = 0
+start_number = 1
+check_list = []
 
-    while stack:
-        stack.pop(0)
-        y = numbers.pop()
-        stack.append(y)
-        visited[n + y] = True
-        DFS(y)
-        stack.pop(0)
-        numbers.append(y)
+while True:
+    x = check_list.copy()
 
+    for i in range(len(x)):
+        x[i] += numbers[idx]
 
-DFS(numbers[0])
+    check_list.append(numbers[idx])
+    check_list = check_list + x
 
-for i in range(len(visited)):
-    if not visited[i]:
-        print(i)
+    if start_number not in set(check_list):
+        print(start_number)
+        break
+    else:
+        idx += 1
+        start_number += 1
+
+        if idx == len(numbers):
+            break
+
+while True:
+    if start_number not in set(check_list):
+        print(start_number)
+        break
+    else:
+        start_number += 1
 
