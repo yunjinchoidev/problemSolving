4d84975beb668b7a35dd2910948ff4198b324456
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-18 17:18:24 +0900

    [백준] 강의실 배정 -> heapq 를 이용한 그리디 알고리즘 풀이

--- 2023/3월/[백준][11000][강의실배정].py
+++ 2023/3월/[백준][11000][강의실배정].py
@@ -1,5 +1,6 @@
 import sys
 import time
+import heapq
 
 input = sys.stdin.readline
 
@@ -7,10 +8,49 @@ start_time = time.time()
 
 N = int(input())
 time_table = []
-for i in range(N):
-    time_table.append(list(map(int, input().split())))
+h = []
 
-print(time_table)
+# 힙을 이용해 시작 시간, 종료시간을 저장한다.
+for i in range(N):
+    s, e = list(map(int, input().split()))
+    heapq.heappush(h, (s, e))
+
+lectureing = []
+max_lecture_cnt = 0
+
+# max_lecture_cnt += 1
+# heapq.heappush(lectureing, h[0][1])
+# heapq.heappop(h)
+
+while True:
+
+    if not h :
+        break
+
+    if lectureing:
+        min_end = lectureing[0]
+
+        input_s, input_e = h[0]
+        if input_s < min_end:
+            if len(lectureing) >= max_lecture_cnt:
+                max_lecture_cnt += 1
+
+            heapq.heappush(lectureing, h[0][1])
+            heapq.heappop(h)
+        else:
+            # lectureing = []
+            heapq.heappop(lectureing)
+            # heapq.heappop(h)
+    else:
+        max_lecture_cnt += 1
+        heapq.heappush(lectureing, h[0][1])
+        heapq.heappop(h)
+
+
+# print(h)
+
+print(max_lecture_cnt)
+
 
 end_time = time.time()
 print("WorkingTime: {} sec".format(end_time - start_time))
