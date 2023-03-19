4b60925f2cd094d34bb2b3ea46e48bb6fe195afa
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-19 22:53:41 +0900

    [이코테] 게임 개발 코드 수정

--- 2023/3월/[이코테] 구현/[이코테] 게임 개발.py
+++ 2023/3월/[이코테] 구현/[이코테] 게임 개발.py
@@ -3,26 +3,34 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+
+# 행, 열, 방향
 row, column, direction = map(int, input().split())
 
 map = [list(map(int, input().split())) for i in range(N)]
 answer = 1
+
+# 방문 여부 체크 배열
 visited = [[False for j in range(M)] for i in range(N)]
 visited[row][column] = True
 
-# print(map)
-
-# 북, 서, 남, 동 순
+# 북, 서, 남, 동
 move_row = [-1, 0, 1, 0]
 move_column = [0, -1, 0, 1]
 
+
+# 바다인 것은 방문 불가능하므로 이미 방문했다고 체크한다.
 for i in range(N):
     for j in range(M):
         if map[i][j] == 1:
             visited[i][j] = True
 
+
 while True:
-    # print(answer)
+
+    '''
+    STEP 1 
+    '''
     # 방향 반시계 방향 90도.
     direction = (direction + 1) % 4
 
@@ -32,6 +40,7 @@ while True:
     nrow = row + move_row[direction]
     ncolumn = column + move_column[direction]
 
+    # 동서남북 돌면서 사방이 막혀있는지 확인
     for i in range(4):
         nrow = row + move_row[(direction + i) % 4]
         ncolumn = column + move_column[(direction + i) % 4]
@@ -43,10 +52,11 @@ while True:
         if map[nrow][ncolumn] == 1 or visited[nrow][ncolumn]:
             block_cnt += 1
 
+    # 사방이 모두 막힌 경우 all_block 체크
     if block_cnt >= 4:
         all_block = True
 
-    # 사방이 막혀있는 경우.
+    # step3 : 사방이 막혀있는 경우(가본곳 or 바다) 바라보고 있는 방향에서 뒤로 이동한다.
     if all_block:
         row = row - move_row[direction]
         column = column - move_column[direction]
@@ -55,20 +65,21 @@ while True:
             break
         visited[row][column] = True
 
-
+        # 이 때 뒤 칸이 바다였다면 멈추고 종료 (끝)
         if map[row][column] == 1:
             break
 
+        # 육지라면 다시 시뮬레이션 진행
         continue
 
+    '''
+    STEP 2
+    '''
     if map[nrow][ncolumn] == 0:
-
         if ncolumn < 0 or ncolumn >= M or nrow < 0 or nrow >= N:
             break
-        print(nrow, ncolumn, block_cnt, direction, answer, visited[nrow][ncolumn], "~~~~~~~~~~~~~~~~")
 
         if not visited[nrow][ncolumn]:
-
             row = nrow
             column = ncolumn
             answer += 1
