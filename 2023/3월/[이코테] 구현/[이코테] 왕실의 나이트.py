66cf18e9fcee29884bf3cd7d6bb5b24fcab65304
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-19 20:49:17 +0900

    [이코테] 왕실의 나이트 4가지 경우가 아니라 8가지 경우임

--- 2023/3월/[이코테] 구현/[이코테] 왕실의 나이트.py
+++ 2023/3월/[이코테] 구현/[이코테] 왕실의 나이트.py
@@ -6,18 +6,22 @@ position = input()
 answer = 0
 
 col, row = position[0], int(position[1])
-print(col, row)
 
+# 영어 소문자 숫자로 바꾸기
 col = ascii_lowercase.index(col) + 1
 
-move_col = [2, 2, 1, -1]
-move_row = [1, -1, 2, 2]
+# 이동할 수 있는 경우
+move_col = [2, 2, -2, -2, 1, -1, 1, -1]
+move_row = [1, -1, 1, -1, 2, 2, -2, -2]
 
-for i in range(4):
+for i in range(8):
     ncol = col + move_col[i]
     nrow = row + move_row[i]
+
+    # 밖으로 나가는 경우
     if ncol < 1 or ncol > 8 or nrow < 1 or nrow > 8:
         continue
+
     answer += 1
 
 print(answer)
