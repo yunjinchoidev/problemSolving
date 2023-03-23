3aa99de8006e08c70e3ce85ec202ec36174e9fd0
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-23 20:40:16 +0900

    [이코테] 기둥과 보 설치

--- 2023/3월/[이코테] 구현/[이코테] 기둥과 보 설치.py
+++ 2023/3월/[이코테] 구현/[이코테] 기둥과 보 설치.py
@@ -8,10 +8,12 @@ def solution(n, build_frame):
         for material in update_map:
             # 보 라면
             if material[2] == 1:
+
                 # 한쪽에라도 기둥이 있으면 된다.
                 if (material[0], material[1] - 1, 0) in update_map or \
                         (material[0] + 1, material[1] - 1, 0) in update_map:
                     continue
+
                 # 양쪽에 보가 있으면 된다.
                 if (material[0] - 1, material[1], 1) in update_map and \
                         (material[0] + 1, material[1], 1) in update_map:
@@ -41,41 +43,26 @@ def solution(n, build_frame):
 
     for frame in build_frame:
 
-        # 설치한다면
-        if frame[2] == 0:
-            if frame[3] == 1:
-                update_map.add((frame[0], frame[1], frame[2]))
+        if frame[3] == 1:
+            update_map.add((frame[0], frame[1], frame[2]))
 
-                if not is_normal(update_map):
-                    update_map.remove((frame[0], frame[1], frame[2]))
-
-            # 삭제한다면
-            if frame[3] == 0:
-
+            if not is_normal(update_map):
                 update_map.remove((frame[0], frame[1], frame[2]))
 
-                if not is_normal(update_map):
-                    update_map.add((frame[0], frame[1], frame[2]))
+        # 삭제한다면
+        if frame[3] == 0:
 
-        if frame[2] == 1:
-            if frame[3] == 1:
+            update_map.remove((frame[0], frame[1], frame[2]))
+
+            if not is_normal(update_map):
                 update_map.add((frame[0], frame[1], frame[2]))
-
-                if not is_normal(update_map):
-                    update_map.remove((frame[0], frame[1], frame[2]))
-
-            # 삭제한다면
-            if frame[3] == 0:
-
-                update_map.remove((frame[0], frame[1], frame[2]))
-
-                if not is_normal(update_map):
-                    update_map.add((frame[0], frame[1], frame[2]))
 
     r = []
     for f in update_map:
         r.append(list(f))
+
     r.sort(key=lambda x: (x[0], x[1], x[2]))
 
     answer = r
+
     return answer
