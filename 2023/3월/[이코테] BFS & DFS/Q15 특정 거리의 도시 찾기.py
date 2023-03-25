0e60b3770c14a9db3d1791fa99db558ad163b62c
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-25 10:38:16 +0900

    [이코테] 특정 거리의 도시 찾기

--- 2023/3월/[이코테] BFS & DFS/Q15 특정 거리의 도시 찾기.py
+++ 2023/3월/[이코테] BFS & DFS/Q15 특정 거리의 도시 찾기.py
@@ -1,1 +1,46 @@
-N, M
+from collections import deque
+import sys
+sys = sys.stdin.readline
+
+N, M, K, X = map(int, input().split())
+
+visited = [False for i in range(N + 1)]
+
+adj = [[] for i in range(N + 1)]
+
+for i in range(M):
+    s, e = map(int, input().split())
+    adj[s].append((e))
+
+
+
+def BFS(s, distance):
+    d = deque()
+    d.append((s, distance))
+    visited[s] = True
+    result = []
+
+    while d:
+        x, dis = d.popleft()
+
+        if dis == K:
+            result.append(x)
+
+        if dis > K:
+            return result
+
+        for p in adj[x]:
+            if not visited[p]:
+                d.append((p, dis + 1))
+                visited[p] = True
+
+    return result
+
+
+result = BFS(X,0)
+
+if len(result) > 0 :
+    for r in result:
+        print(r)
+else:
+    print(-1)
