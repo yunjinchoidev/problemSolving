a51c1f676f979c542d126bffc80f02582ef41aca
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-27 23:24:29 +0900

    [이코테] 감시피하기 주석 추가

--- 2023/3월/[이코테] BFS & DFS/Q20 감시 피하기.py
+++ 2023/3월/[이코테] BFS & DFS/Q20 감시 피하기.py
@@ -14,6 +14,7 @@ dc = [0, 1, 0, -1]
 
 
 def DFS(row, col, map_):
+
     visited = [[False for c in range(N)] for r in range(N)]
     stack = []
     stack.append((row, col))
@@ -22,7 +23,10 @@ def DFS(row, col, map_):
     while stack:
         r, c = stack.pop()
 
+        # 디렉션
         for i in range(4):
+
+            # 뎁스가 계속 들어가는 것임.
             for dep in range(1, N + 1):
 
                 nr = r + dr[i] * dep
@@ -31,19 +35,25 @@ def DFS(row, col, map_):
                 if nr < 0 or nr >= N or nc < 0 or nc >= N:
                     continue
 
+                # 해당 방향에 장애물이 존재하면 해당 방향으로 더 이상 감시 불가
                 if map_[nr][nc] == 'O':
                     break
 
+                # 학생 감시 성공
                 if map_[nr][nc] == 'S':
                     return False
 
+                # 방문 표시
                 if not visited[nr][nc]:
                     visited[nr][nc] = True
 
     return True
 
 
+# 브루트 포스
 for comb in combis:
+
+    # 장애물을 설치
     copy_map_ = copy.deepcopy(map_)
     copy_map_[comb[0][0]][comb[0][1]] = 'O'
     copy_map_[comb[1][0]][comb[1][1]] = 'O'
@@ -51,6 +61,7 @@ for comb in combis:
 
     answer = 'YES'
 
+    # 맵 내의 모든 선생님들이 감시할 수 있는 범위를 DFS 를 통해 조사.
     for r in range(N):
         for c in range(N):
             if copy_map_[r][c] == 'T':
@@ -59,6 +70,7 @@ for comb in combis:
         if answer == 'NO':
             break
 
+    # 어떤 장애물의 조합이 모든 선생님들이 단 한번도 감시를 성공하지 못한 경우가 발생한 경우.
     if answer == 'YES':
         break
 
