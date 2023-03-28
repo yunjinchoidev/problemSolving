5691a83c3a818f14d1cdf80a20dbb8c4c2dc706f
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-28 09:24:01 +0900

    [이코테] 특정 거리의 도시, 연구소 코드 수정

--- 2023/3월/[이코테] BFS & DFS/Q15 특정 거리의 도시 찾기.py
+++ 2023/3월/[이코테] BFS & DFS/Q15 특정 거리의 도시 찾기.py
@@ -1,16 +1,18 @@
 from collections import deque
 import sys
+
 input = sys.stdin.readline
 
 N, M, K, X = map(int, input().split())
 
 visited = [False for i in range(N + 1)]
 
-adj = [[] for i in range(N + 1)]
+adjacent = [[] for i in range(N + 1)]
 
+# 인접 리스트
 for i in range(M):
     s, e = map(int, input().split())
-    adj[s].append((e))
+    adjacent[s].append((e))
 
 
 def BFS(s, distance):
@@ -22,23 +24,27 @@ def BFS(s, distance):
     while d:
         x, dis = d.popleft()
 
+        # 특정 거리인 것들을 result 에 담아주기
         if dis == K:
             result.append(x)
 
-        if dis > K:
+        # K+1 이상이 되면 BFS 탐색 종료
+        if dis >= K+1:
             return result
 
-        for p in adj[x]:
-            if not visited[p]:
-                d.append((p, dis + 1))
-                visited[p] = True
+        for adj in adjacent[x]:
+            if not visited[adj]:
+                d.append((adj, dis + 1))
+                visited[adj] = True
 
     return result
 
 
-result = BFS(X,0)
+result = BFS(X, 0)
 result.sort()
-if len(result) > 0 :
+
+
+if len(result) > 0:
     for r in result:
         print(r)
 else:
--- 2023/3월/[이코테] BFS & DFS/Q16 연구소.py
+++ 2023/3월/[이코테] BFS & DFS/Q16 연구소.py
@@ -9,127 +9,61 @@ from collections import deque
 N, M = map(int, input().split())
 map_ = [list(map(int, input().split())) for row in range(N)]
 
-coords = [(r, c) for c in range(M) for r in range(N) if map_[r][c] == 0]
-result = list(itertools.combinations(coords, 3))
+# 벽을 세울 수 있는 위치
+possible_position = [(r, c) for c in range(M) for r in range(N) if map_[r][c] == 0]
+position_combinations = list(itertools.combinations(possible_position, 3))
 
 # 북동남서
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
+answer = -float('inf')
 
-# for r in range(N):
-#     for c in range(M):
-#         if map_[r][c] == 0:
-#             visited[r][c] = True
 
-max_zero = -float('inf')
-for c in result:
+def BFS(sr, sc):
+    global new_map, new_visited
+
+    d = deque()
+    d.append((sr, sc))
+    new_visited[sr][sc] = True
+
+    while d:
+        r, c = d.popleft()
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr < 0 or nr >= N or nc < 0 or nc >= M:
+                continue
+
+            if new_map[nr][nc] == 0 and not new_visited[nr][nc]:
+                new_map[nr][nc] = 2
+                d.append((nr, nc))
+
+# 모든 조합에 대해서 BFS 를 수행
+for combi in position_combinations:
+
+    # 안전영역 수
+    safe_zone_cnt = 0
+
+    # 새로운 맵 생성
     new_map = copy.deepcopy(map_)
     new_visited = visited = [[False for col in range(M)] for row in range(N)]
-    new_map[c[0][0]][c[0][1]] = 1
-    new_map[c[1][0]][c[1][1]] = 1
-    new_map[c[2][0]][c[2][1]] = 1
+    new_map[combi[0][0]][combi[0][1]] = 1
+    new_map[combi[1][0]][combi[1][1]] = 1
+    new_map[combi[2][0]][combi[2][1]] = 1
 
-    # print(new_map)
-
-
-    def BFS(sr, sc):
-        global new_map, new_visited
-
-        d = deque()
-        d.append((sr, sc))
-        new_visited[sr][sc] = True
-
-        while d:
-            r, c = d.popleft()
-
-            for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-
-                if nr < 0 or nr >= N or nc < 0 or nc >= M:
-                    continue
-
-                if new_map[nr][nc] == 0 and not new_visited[nr][nc]:
-                    new_map[nr][nc] = 2
-                    d.append((nr, nc))
-
-
-    # pprint(new_map)
-    # pprint(new_visited)
+    # 바이러스가 완전히 퍼졌을 때
     for r in range(N):
         for c in range(M):
             if new_map[r][c] == 2 and not new_visited[r][c]:
                 BFS(r, c)
 
-    # pprint(new_map)
-    # pprint(new_visited)
-    c = 0
 
+    # 바이러스가 완전 탐색을 마쳤으므로 안전영역의 갯수는 단순히 0 의 개수다.
     for row in new_map:
-        c += row.count(0)
-    max_zero = max(c, max_zero)
+        safe_zone_cnt += row.count(0)
+    answer = max(safe_zone_cnt, answer)
 
-print(max_zero)
-
-
-#
-#
-#
-# from itertools import combinations
-# import copy
-# from collections import deque
-#
-# N, M = map(int, input().split())
-# map_ = [list(map(int, input().split())) for row in range(N)]
-#
-# coords = [(r, c) for c in range(M) for r in range(N) if map_[r][c] == 0]
-# result = list(combinations(coords, 3))
-#
-# dr = [-1, 0, 1, 0]
-# dc = [0, 1, 0, -1]
-#
-# max_zero = -float('inf')
-# for c in result:
-#     new_map = copy.deepcopy(map_)
-#     new_visited = [[False for col in range(M)] for row in range(N)]
-#     new_map[c[0][0]][c[0][1]] = 1
-#     new_map[c[1][0]][c[1][1]] = 1
-#     new_map[c[2][0]][c[2][1]] = 1
-#
-#     def BFS(sr, sc):
-#         global new_map, new_visited
-#
-#         d = deque()
-#         d.append((sr, sc))
-#         new_visited[sr][sc] = True
-#
-#         while d:
-#             r, c = d.popleft()
-#
-#             for i in range(4):
-#                 nr = r + dr[i]
-#                 nc = c + dc[i]
-#
-#                 if nr < 0 or nr >= N or nc < 0 or nc >= M:
-#                     continue
-#
-#                 if new_map[nr][nc] == 0 and not new_visited[nr][nc]:
-#                     new_map[nr][nc] = 2
-#                     new_visited[nr][nc] = True
-#                     d.append((nr, nc))
-#
-#     for r in range(N):
-#         for c in range(M):
-#             if new_map[r][c] == 2 and not new_visited[r][c]:
-#                 BFS(r, c)
-#
-#     c = 0
-#     for row in new_map:
-#         c += row.count(0)
-#     max_zero = max(c, max_zero)
-#
-# print(max_zero)
-#
-#
-#
+print(answer)
