96fbb5ee083794d577d1141adc1e16683e12c01b
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-03-30 09:45:37 +0900

    [이코테] 감시 피하기 코드 수정

--- 2023/3월/[이코테] BFS & DFS/Q20 감시 피하기.py
+++ 2023/3월/[이코테] BFS & DFS/Q20 감시 피하기.py
@@ -6,72 +6,70 @@ N = int(input())
 map_ = [list(map(str, input().split())) for i in range(N)]
 
 coords = [(r, c) for c in range(N) for r in range(N) if map_[r][c] == 'X']
-combis = list(itertools.combinations(coords, 3))
+combinations = list(itertools.combinations(coords, 3))
 
 # 북동남서
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
 
-def DFS(row, col, map_):
-
-    visited = [[False for c in range(N)] for r in range(N)]
+def is_caught_dfs(row, col, map_):
     stack = []
     stack.append((row, col))
-    visited[row][col] = True
 
     while stack:
+
         r, c = stack.pop()
 
-        # 디렉션
         for i in range(4):
 
-            # 뎁스가 계속 들어가는 것임.
-            for dep in range(1, N + 1):
+            # 멀리볼 수 있으므로 감시가 map_의 상하좌우 끝 까지 간다. 이를 watch_depth 로 실행.
+            for watch_depth in range(1, N + 1):
 
-                nr = r + dr[i] * dep
-                nc = c + dc[i] * dep
+                nr = r + dr[i] * watch_depth
+                nc = c + dc[i] * watch_depth
 
                 if nr < 0 or nr >= N or nc < 0 or nc >= N:
                     continue
 
-                # 해당 방향에 장애물이 존재하면 해당 방향으로 더 이상 감시 불가
+                # 해당 방향에 장애물이 존재하면 해당 방향으로 더 이상 감시 종료
                 if map_[nr][nc] == 'O':
                     break
 
-                # 학생 감시 성공
+                # 학생이 감시에 걸림.
                 if map_[nr][nc] == 'S':
-                    return False
+                    return True  # 걸렸다
 
-                # 방문 표시
-                if not visited[nr][nc]:
-                    visited[nr][nc] = True
-
-    return True
+    # 여기 까지 왔다면 아무도 걸리지 않았다는 것.
+    return False  # 안걸렸다.
 
 
-# 브루트 포스
-for comb in combis:
+# 완전 탐색
+for combination in combinations:
 
-    # 장애물을 설치
-    copy_map_ = copy.deepcopy(map_)
-    copy_map_[comb[0][0]][comb[0][1]] = 'O'
-    copy_map_[comb[1][0]][comb[1][1]] = 'O'
-    copy_map_[comb[2][0]][comb[2][1]] = 'O'
+    # 장애물 설치
+    map_[combination[0][0]][combination[0][1]] = 'O'
+    map_[combination[1][0]][combination[1][1]] = 'O'
+    map_[combination[2][0]][combination[2][1]] = 'O'
 
     answer = 'YES'
 
     # 맵 내의 모든 선생님들이 감시할 수 있는 범위를 DFS 를 통해 조사.
     for r in range(N):
         for c in range(N):
-            if copy_map_[r][c] == 'T':
-                if not DFS(r, c, copy_map_):
+            if map_[r][c] == 'T':
+                if is_caught_dfs(r, c, map_):  # 걸렸다면
                     answer = 'NO'
         if answer == 'NO':
             break
 
-    # 어떤 장애물의 조합이 모든 선생님들이 단 한번도 감시를 성공하지 못한 경우가 발생한 경우.
+    # 한 번도 감시에 걸리지 않았다면 56행의 answer = 'YES' 가 보존 되었을 것임.
     if answer == 'YES':
         break
+
+    # 원복
+    map_[combination[0][0]][combination[0][1]] = 'X'
+    map_[combination[1][0]][combination[1][1]] = 'X'
+    map_[combination[2][0]][combination[2][1]] = 'X'
 
 print(answer)
