098ab7936956b1fd3679cd3b58759e82fbda76a9
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-03 08:35:20 +0900

    [이코테] 성적이 낮은 순서로 학생 출력하기

---
+++ 2023/4월/이코테/정렬/[이코테] 성적이 낮은 순서로 학생 출력하기.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+students = []
+for i in range(N):
+    name, score = map(str, input().split())
+    students.append(name, int(score))
+
+students.sort(key=lambda x: (x[1]))
+
+for student in students:
+    print(student[0], end=' ')
