6c2b02e5b79814f01b83c78c45e1e26d30c5b367
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-07 20:33:39 +0900

    [이코테] 두 배열의 합 -> 해시맵 dict

---
+++ 2023/4월/백준 2143.py
@@ -0,0 +1,51 @@
+from collections import Counter
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+N = int(input())
+array1 = list(map(int, input().split()))
+M = int(input())
+array2 = list(map(int, input().split()))
+
+prefix_A = []
+
+for i in range(N):
+    for j in range(i, N, 1):
+        prefix_A.append(sum(array1[i:j + 1]))
+
+prefix_B = []
+for i in range(M):
+    for j in range(i, M, 1):
+        prefix_B.append(sum(array2[i:j+1]))
+
+# print(prefix_A)
+# print(prefix_B)
+
+# prefix_A.sort()
+# prefix_B.sort()
+# def binary_search(array, target, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if array[mid] == target:
+#             return mid
+#         elif array[mid] > target:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return None
+
+countor_prefix_A = Counter(prefix_A)
+countor_prefix_B = Counter(prefix_B)
+# print(countor_prefix_A)
+# print(countor_prefix_B)
+
+answer = 0
+for key, value in countor_prefix_A.items():
+    # m = binary_search(prefix_B, T - key, 0, len(prefix_B) - 1)
+
+    # print(key, countor_prefix_B[T - key] )
+    if T - key in countor_prefix_B:
+        answer += value * countor_prefix_B[T - key]
+
+print(answer)
