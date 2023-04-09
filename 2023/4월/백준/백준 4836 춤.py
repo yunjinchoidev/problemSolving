70cc8ae413b3f01f404a3ac3c6421c8c55695fa3
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-09 16:11:05 +0900

    [백준] 1로 만들기

--- 2023/4월/백준/백준 4836 춤.py
+++ 2023/4월/백준/백준 4836 춤.py
@@ -1,36 +1,36 @@
-while True:
-    try:
-        dances = input().split()
-        is_normal = False
-        for i in range(len(dances)):
-            if dances[i] == 'dip':
-                if i != 0:
-                    if dances[i-1] == 'jiggle':
-                        is_normal = True
-                        continue
-                if i > 1:
-                    if dances[i-2] == 'jiggle':
-                        is_normal = True
-                        continue
-
-        dances[-1] = 'clap'
-        dances[-2] = 'stomp'
-        dances[-3] = 'clap'
-
-        for i in range(len(dances)-1):
-            if dances[i] == 'twirl':
-                if dances[i+1] != 'hop':
-                    is_normal = False
-                    break
-
-
-        if dances[0] != 'jiggle':
-            is_normal = False
-
-        if dances.count('dip') == 0:
-
-
-
-
-    except:
-        break
+# while True:
+#     try:
+#         dances = input().split()
+#         is_normal = False
+#         for i in range(len(dances)):
+#             if dances[i] == 'dip':
+#                 if i != 0:
+#                     if dances[i-1] == 'jiggle':
+#                         is_normal = True
+#                         continue
+#                 if i > 1:
+#                     if dances[i-2] == 'jiggle':
+#                         is_normal = True
+#                         continue
+#
+#         dances[-1] = 'clap'
+#         dances[-2] = 'stomp'
+#         dances[-3] = 'clap'
+#
+#         for i in range(len(dances)-1):
+#             if dances[i] == 'twirl':
+#                 if dances[i+1] != 'hop':
+#                     is_normal = False
+#                     break
+#
+#
+#         if dances[0] != 'jiggle':
+#             is_normal = False
+#
+#         if dances.count('dip') == 0:
+#
+#
+#
+#
+#     except:
+#         break
