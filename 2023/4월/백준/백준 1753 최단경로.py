7c801716d0d0074494eb01bf9e26ae492c97b49c
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-09 19:07:38 +0900

    [백준] 최단경로 -> 다익스트라

---
+++ 2023/4월/백준/백준 1753 최단경로.py
@@ -0,0 +1,29 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    dis[start] = 0
+    while q:
+        d, now = heapq.heappop(q)
+        if dis[now] < d:
+            continue
+        for v, w in graph[now]:
+            cost = d + w
+            if cost < dis[v]:
+                dis[v] = cost
+                heapq.heappush(q, (cost, v))
+
+V, E = map(int, input().split())
+start = int(input())
+INF = 10**9
+dis = [INF]*(V+1)
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+dijkstra(start)
+for n in dis[1:]:
+    print(n if n != INF else "INF")
