d2beb08a2aace5164144a4cb5b178a433bd3b5e8
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-10 09:24:08 +0900

    [이코테] 개미전사 코드 수정

--- 2023/4월/이코테/DP/개미전사.py
+++ 2023/4월/이코테/DP/개미전사.py
@@ -1,43 +1,17 @@
-from collections import deque
 import sys
-
 input = sys.stdin.readline
 
 N = int(input())
-
 numbers = list(map(int, input().split()))
 
-answer = 0
-
-idx = 0
-
-left = numbers[:idx + 1]
-right = numbers[idx + 2:]
-
-print(left, right)
-
-lm = max(left)
-rm = max(right)
-
 r = []
-r.append(lm+ rm)
 
-left_dp = [0 for i in range(N)]
-right_dp = [0 for i in range(N)]
+answer = float('-inf')
 
-for i in range(1, N - 2):
+# 한 지점을 기준으로 최대로 약탈가능한 것을 구한다.
+for i in range(0, N - 2):
     lm = max(numbers[:i + 1])
     rm = max(numbers[i + 2:])
-    r.append(rm + lm)
+    answer = max(answer, lm + rm)
 
-print(r)
-print(max(r))
-
-rr = []
-for i in range(1, N -2):
-    left_dp[i] = max(left_dp[i-1], numbers[i])
-    right_dp[i] = max(right_dp[i-1], numbers[i])
-    rr.append(left_dp[i] + right_dp[i])
-
-print(rr)
-print(max(rr))
+print(answer)
