6beb25c5da925738dddacc9ddd2a92f296e67c8e
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-10 23:32:32 +0900

    [이코테] 효율적인 화폐 구성

--- 2023/4월/이코테/DP/효율적인 화폐 구성.py
+++ 2023/4월/이코테/DP/효율적인 화폐 구성.py
@@ -2,21 +2,21 @@ N, M = map(int, input().split())
 moneys = [int(input()) for i in range(N)]
 moneys.sort(reverse=True)
 
-dp = [9999999 for i in range(M+1)]
-dp[0] = 0
+dp = [-1 for i in range(100001)]
+dp[0] = -1
 
 for money in moneys:
     dp[money] = 1
 
-for i in range(max(moneys)+1, M+1):
-    flag = False
+for i in range(moneys[0]+1, M+1):
+    x = float('inf')
     for money in moneys:
-        if i % money == 0:
-            dp[i] = dp[i//money]+1
-            flag = True
-            break
-    if not flag:
+        if dp[i-money] != -1 : # 만들 수 있다는 것.
+            x = min(x, dp[i-money])
+
+    if x != float('inf'):
+        dp[i] = x + 1
+    else:
         dp[i] = -1
 
-print(dp)
 print(dp[M])
