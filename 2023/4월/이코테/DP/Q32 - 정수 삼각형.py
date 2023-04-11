729c50220aed125a637fc289e669309b178f4b5b
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-11 21:10:41 +0900

    [이코테] 정수 삼각형

---
+++ 2023/4월/이코테/DP/Q32 - 정수 삼각형.py
@@ -0,0 +1,25 @@
+N = int(input())
+triangle = [[] for i in range(N)]
+for i in range(N):
+    triangle[i] = list(map(int, input().split()))
+
+# print(triangle)
+
+dp = [[0 for c in range(len(triangle[r]))] for r in range(N)]
+
+for r in range(N):
+    for j in range(len(triangle[r])):
+        if j == 0:
+            dp[r][j] = triangle[r][j] + dp[r-1][0]
+        elif j == len(triangle[r]) - 1:
+            dp[r][j] = triangle[r][j] + dp[r - 1][len(triangle[r-1]) - 1]
+        else:
+            dp[r][j] = triangle[r][j] + max(dp[r-1][j-1], dp[r-1][j])
+
+
+answer = 0
+for i in range(N):
+    answer = max(answer, dp[N-1][i])
+
+# print(dp)
+print(answer)
