1a4e5ae110203c560a1afc6e64866d6ebc43da1d
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-12 22:44:58 +0900

    [이코테] 병사 배치

---
+++ 2023/4월/이코테/DP/Q34 - 병사 배치하기.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+N = int(input())
+soldiers = list(map(int, input().split()))
+soldiers.insert(0, 0)
+
+dp = [1 for i in range(2001)]
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2, N+1):
+    for j in range(1, i+1):
+        if soldiers[i-j] <= soldiers[i]: # 뒤쪽 군인이 군사력이 더 크다면
+            continue
+        else: # 정상인 경우
+            dp[i] = max(dp[i], dp[i - j]+1)
+
+
+# print(dp)
+print(N-dp[N])
