048e49eb6c6c965b9b1e3bf9b3ebe2835508bbef
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-13 10:34:23 +0900

    [이코테] 병사 배치, 퇴사 주석 추가

--- 2023/4월/이코테/DP/Q33 - 퇴사.py
+++ 2023/4월/이코테/DP/Q33 - 퇴사.py
@@ -9,11 +9,12 @@ schedules.insert(0, [0, 0])
 
 dp = [0 for i in range(N + 1)]
 
-# i 번째 날을 사용해야 하는 날짜들 중에서 최댓값을 더한 것이 dp[i] 다.
+
+# 이중 for문으로 날짜를 늘려가면서 최댓값을 갱신해나간다.
 for i in range(1, len(schedules)):
     dp[i] = dp[i - 1]
     for j in range(1, i+1):
-        if j + schedules[j][0] - 1 == i:
+        if j + schedules[j][0] - 1 == i: # i번 째 날에 신규로 포함 시킬 수 있는 상담일에 대해서 dp 를 갱신 처리
             dp[i] = max(dp[i], dp[i - schedules[j][0]] + schedules[j][1])
 
 print(dp[-1])
--- 2023/4월/이코테/DP/Q34 - 병사 배치하기.py
+++ 2023/4월/이코테/DP/Q34 - 병사 배치하기.py
@@ -13,13 +13,15 @@ dp = [1 for i in range(2001)]
 dp[0] = 0
 dp[1] = 1
 
-for i in range(2, N+1):
-    for j in range(1, i+1):
-        if soldiers[i-j] <= soldiers[i]: # 뒤쪽 군인이 군사력이 더 크다면
+
+for i in range(2, N + 1):
+
+    # 새롭게 추가되는 병사 기준으로 왼쪽으로 반복문을 진행하면서 dp 를 갱신해나간다.
+    for j in range(1, i + 1):
+        if soldiers[i - j] <= soldiers[i]:  # 뒤쪽 군인이 군사력이 더 크다면 오름차순이 되므로 체크 안함.
             continue
-        else: # 정상인 경우
-            dp[i] = max(dp[i], dp[i - j]+1)
+        else:  # 정상 내림차순인 경우 해당 지점의 dp와 비교.
+            dp[i] = max(dp[i], dp[i - j] + 1)
 
-
-# print(dp)
-print(N-max(dp))
+# 각 지점의 dp 가 가지고 있는 값은 최대 내림차순 길이이므로 max 값을 때주면 최소 열외 인원을 구할 수 있음.
+print(N - max(dp))
