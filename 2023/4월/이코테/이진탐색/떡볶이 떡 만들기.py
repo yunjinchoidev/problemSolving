950c069bf834fd8efc4520cc9bccf805849e9811
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-16 20:45:40 +0900

    [이코테] 떡볶이 떡 만들기

--- 2023/4월/이코테/이진탐색/떡볶이 떡 만들기.py
+++ 2023/4월/이코테/이진탐색/떡볶이 떡 만들기.py
@@ -6,28 +6,20 @@ height = numbers[0]
 idx = 1
 s = 0
 answer = 0
-# for i in range(height-1, 1, -1):
-#     if i < numbers[idx]:
-#         idx +=1
-#         s += idx
-#     else:
-#         s += idx
-#
-#     print(i, s, idx)
-#     if s >= M:
-#         answer = i
-#         break
-#
-# print(answer)
 
 def binary_search(start, end, numbers, M):
     if start > end:
         return None
     mid = (start+end)//2
+
+
     s = 0
     for number in numbers:
         if number > mid:
             s += number - mid
+
+
+
     if s >= M:
         return binary_search(mid+1, end, numbers, M)
     else:
