2079a838af615e4ad702ce7ba822888df883f634
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-17 22:37:59 +0900

    [이코테] 정렬된 배열에서 특정 수의 개수 구하기

--- 2023/4월/이코테/이진탐색/Q27 - 정렬된 배열에서 특정 수의 개수 구하기.py
+++ 2023/4월/이코테/이진탐색/Q27 - 정렬된 배열에서 특정 수의 개수 구하기.py
@@ -7,9 +7,10 @@ N, x = map(int, input().split())
 
 numbers = list(map(int, input().split()))
 
+# bisect
 answer = bisect.bisect_right(numbers, x) - bisect.bisect_left(numbers, x)
 
-if answer > 0 :
+if answer > 0:
     print(answer)
 else:
     print(-1)
--- 2023/4월/이코테/이진탐색/Q28 - 고정점 찾기.py
+++ 2023/4월/이코테/이진탐색/Q28 - 고정점 찾기.py
@@ -1,7 +1,9 @@
 import sys
+
 input = sys.stdin.readline
 
-def binary_search(array, target, start, end):
+
+def binary_search(array, start, end):
     while start <= end:
         mid = (start + end) // 2
 
@@ -14,7 +16,8 @@ def binary_search(array, target, start, end):
 
     return -1
 
+
 N = int(input())
 numbers = list(map(int, input().split()))
 
-print(binary_search(numbers, 0, 0, N-1))
+print(binary_search(numbers, 0, N - 1))
