4a7e4cff41b2432dbd4560b9d6a0f2df7bd73ddc
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-17 22:32:09 +0900

    [이코테] 고정점찾기

---
+++ 2023/4월/이코테/이진탐색/Q28 - 고정점 찾기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == mid:
+            return mid
+        elif array[mid] > mid:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return -1
+
+N = int(input())
+numbers = list(map(int, input().split()))
+
+print(binary_search(numbers, 0, 0, N-1))
