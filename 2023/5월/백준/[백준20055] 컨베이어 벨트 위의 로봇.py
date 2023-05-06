cb43a37b56adfbcc42eca2bd6dfde71d0af824c1
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-05-06 11:35:08 +0900

    [이코테] 컨베이어 벨트

---
+++ 2023/5월/백준/[백준20055] 컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,57 @@
+import sys
+from collections import deque
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+N, K = map(int, input().split())
+
+numbers = list(map(int, input().split()))
+
+# print(numbers)
+
+d = deque(numbers)
+
+robo = [False for i in range(N)]
+robots = deque(robo)
+
+idx = 0
+while True:
+
+    idx += 1
+
+    # step 1
+    d.rotate()
+    robots.rotate()
+
+
+    robots[N-1] = False
+
+
+    for i in range(N-2, -1, -1):
+        if robots[i] and not robots[i + 1] and d[i + 1] >= 1:
+            # print(i)
+            robots[i] = False
+            robots[i + 1] = True
+            d[i + 1] -= 1
+
+            robots[N-1] = False
+
+    if d[0] != 0 and not robots[0]:
+        robots[0] = True
+        d[0] -= 1
+
+    # print(robots)
+    # print(d)
+
+    # if idx == 10:
+    #     break
+    if d.count(0) >= K:
+        break
+
+    # print(idx)
+
+# print(d)
+print(idx)
