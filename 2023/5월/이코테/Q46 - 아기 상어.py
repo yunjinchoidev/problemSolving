67ffa3c8d22e1467f5fa68b29ea051ce9c300ba2
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-05-08 11:54:59 +0900

    [이코테] 최종 순위, 아기 상어

--- 2023/5월/이코테/Q46 - 아기 상어.py
+++ 2023/5월/이코테/Q46 - 아기 상어.py
@@ -1,3 +1,76 @@
+# 통과 코드
+import sys
+from collections import deque
+N = int(input())
+
+dx = [-1,0,0,1] # 상 좌 우 하
+dy = [0,-1,1,0]
+room = []
+sharksize = 2
+sharkeat = 0
+
+for i in range(N):
+    room.append([int(x) for x in sys.stdin.readline().rstrip().split()])
+    for j in range(len(room[i])):
+        if room[i][j] == 9:
+            room[i][j] = 0
+            shark_x, shark_y = i, j
+
+def finding_fish(sx,sy):
+    global sharksize
+    deq = deque()
+    deq.append([sx,sy])
+
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    distance = [[0 for _ in range(N)] for _ in range(N)]
+    can_eat_fish = []
+
+    while deq:
+        x, y = deq.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if room[nx][ny] <= sharksize and not visited[nx][ny]: #이동이 가능하면
+                    visited[nx][ny] = True
+                    distance[nx][ny] = distance[x][y] + 1
+                    deq.append([nx,ny])
+
+                    if room[nx][ny] < sharksize and room[nx][ny] != 0: #물고기가 있고 그걸 먹을 수 있다면
+                        can_eat_fish.append([nx ,ny,distance[nx][ny]])
+
+    can_eat_fish.sort(key= lambda x : (x[2],x[0],x[1])) # 정렬은 거리, x, y 오름차순으로
+    return can_eat_fish
+
+if __name__ == '__main__':
+    ans = 0
+
+    while True:
+        fishlist = finding_fish(shark_x,shark_y)
+
+        if len(fishlist) == 0: # 먹을 수 있는 물고기가 없다면
+            print(ans)
+            exit(0)
+
+        shark_x, shark_y, move_time = fishlist[0]
+
+        sharkeat += 1
+        if sharksize == sharkeat: #먹은 물고기수와 사이즈가 같다면
+            sharkeat = 0
+            sharksize += 1
+
+        room[shark_x][shark_y] = 0 # 물고기 먹은 자리는 빈칸으로 바꿈
+        ans += move_time
+
+
+
+
+
+
+
+
+
+# 내가 푼 풀이
 # import sys
 # from collections import deque
 #
@@ -122,78 +195,6 @@
 #
 #
 #
-
-
-
-import sys
-from collections import deque
-N = int(input())
-
-dx = [-1,0,0,1] # 상 좌 우 하
-dy = [0,-1,1,0]
-room = []
-sharksize = 2
-sharkeat = 0
-
-for i in range(N):
-    room.append([int(x) for x in sys.stdin.readline().rstrip().split()])
-    for j in range(len(room[i])):
-        if room[i][j] == 9:
-            room[i][j] = 0
-            shark_x, shark_y = i, j
-
-def finding_fish(sx,sy):
-    global sharksize
-    deq = deque()
-    deq.append([sx,sy])
-
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    distance = [[0 for _ in range(N)] for _ in range(N)]
-    can_eat_fish = []
-
-    while deq:
-        x, y = deq.popleft()
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if room[nx][ny] <= sharksize and not visited[nx][ny]: #이동이 가능하면
-                    visited[nx][ny] = True
-                    distance[nx][ny] = distance[x][y] + 1
-                    deq.append([nx,ny])
-
-                    if room[nx][ny] < sharksize and room[nx][ny] != 0: #물고기가 있고 그걸 먹을 수 있다면
-                        can_eat_fish.append([nx ,ny,distance[nx][ny]])
-
-    can_eat_fish.sort(key= lambda x : (x[2],x[0],x[1])) # 정렬은 거리, x, y 오름차순으로
-    return can_eat_fish
-
-if __name__ == '__main__':
-    ans = 0
-
-    while True:
-        fishlist = finding_fish(shark_x,shark_y)
-
-        if len(fishlist) == 0: # 먹을 수 있는 물고기가 없다면
-            print(ans)
-            exit(0)
-
-        shark_x, shark_y, move_time = fishlist[0]
-
-        sharkeat += 1
-        if sharksize == sharkeat: #먹은 물고기수와 사이즈가 같다면
-            sharkeat = 0
-            sharksize += 1
-
-        room[shark_x][shark_y] = 0 # 물고기 먹은 자리는 빈칸으로 바꿈
-        ans += move_time
-
-
-
-
-
-
-
 
 
 
--- 2023/5월/이코테/그래프 이론/Q45 - 최종 순위.py
+++ 2023/5월/이코테/그래프 이론/Q45 - 최종 순위.py
@@ -1,58 +1,4 @@
-# import sys
-# from collections import deque
-#
-# # input = sys.stdin.readline
-#
-# T = int(input())
-#
-# for i in range(T):
-#     n = int(input())
-#     numbers = list(map(int, input().split()))
-#     m = int(input())
-#     changes = [list(map(int, input().split())) for i in range(m)]
-#
-#     adj = [[] for i in range(n + 1)]
-#     indegree = [0] * (n + 1)
-#
-#     for i in range(len(numbers) - 1):
-#         adj[numbers[i]].append(numbers[i + 1])
-#         indegree[numbers[i]] += 1
-#     print(indegree)
-#
-#
-#
-#     for i in range(len(changes)):
-#
-#         if changes[i][0] in adj[changes[i][1]]:
-#             adj[changes[i][1]].remove(changes[i][0])
-#             adj[changes[i][1]].append(numbers[i][1])
-#             indegree[changes[i][1]] -= 1
-#
-#         adj[changes[i][0]].append(changes[i][1])
-#         indegree[changes[i][0]] += 1
-#
-#     result = []
-#     q = deque()
-#
-#     print(indegree)
-#     # 진입차수 0 인 것 다 넣기.
-#     for i in range(1, n + 1):
-#         if indegree[i] == 0:
-#             q.append(i)
-#
-#     while q:
-#         now = q.popleft()
-#         result.append(now)  # result 에 넣어주기
-#
-#         for i in adj[now]:
-#             indegree[i] -= 1
-#
-#             if indegree[i] == 0:
-#                 q.append(i)  # 시간 더해서 넣어주기
-#
-#     print(result)
-
-
+# 통과 풀이
 from collections import deque
 
 # 테스트 케이스(Test Case)만큼 반복
@@ -134,3 +80,58 @@ for tc in range(int(input())):
         print()
 
 
+
+# 내가 푼 풀이
+# import sys
+# from collections import deque
+#
+# # input = sys.stdin.readline
+#
+# T = int(input())
+#
+# for i in range(T):
+#     n = int(input())
+#     numbers = list(map(int, input().split()))
+#     m = int(input())
+#     changes = [list(map(int, input().split())) for i in range(m)]
+#
+#     adj = [[] for i in range(n + 1)]
+#     indegree = [0] * (n + 1)
+#
+#     for i in range(len(numbers) - 1):
+#         adj[numbers[i]].append(numbers[i + 1])
+#         indegree[numbers[i]] += 1
+#     print(indegree)
+#
+#
+#
+#     for i in range(len(changes)):
+#
+#         if changes[i][0] in adj[changes[i][1]]:
+#             adj[changes[i][1]].remove(changes[i][0])
+#             adj[changes[i][1]].append(numbers[i][1])
+#             indegree[changes[i][1]] -= 1
+#
+#         adj[changes[i][0]].append(changes[i][1])
+#         indegree[changes[i][0]] += 1
+#
+#     result = []
+#     q = deque()
+#
+#     print(indegree)
+#     # 진입차수 0 인 것 다 넣기.
+#     for i in range(1, n + 1):
+#         if indegree[i] == 0:
+#             q.append(i)
+#
+#     while q:
+#         now = q.popleft()
+#         result.append(now)  # result 에 넣어주기
+#
+#         for i in adj[now]:
+#             indegree[i] -= 1
+#
+#             if indegree[i] == 0:
+#                 q.append(i)  # 시간 더해서 넣어주기
+#
+#     print(result)
