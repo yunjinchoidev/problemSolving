e7ced7883d91b8f2dc55fcac6cdb524ceb77aa15
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-05-11 10:59:20 +0900

    fix 어른 상어

--- 2023/5월/이코테/기출/Q48 - 어른 상어.py
+++ 2023/5월/이코테/기출/Q48 - 어른 상어.py
@@ -6,9 +6,7 @@ N, M, k = map(int, input().split())
 map_ = [[] for r in range(N)]
 
 for i in range(N):
-
     numbers = list(map(int, input().split()))
-
     for num in numbers:
         if num == 0:
             map_[i].append([0, 0, 0, 0])
@@ -22,22 +20,13 @@ for r in range(N):
         if map_[r][c][0] != 0:
             map_[r][c][1] = init_direction[map_[r][c][0] - 1] - 1  # 1 을 빼서 더해준다.
 
-# 상어 맵
-# 작을 수록 큼.
-# M 개의 상어
-# 냄새 뿌리기.
-# 상하좌우 이동 . 냄새가 없는 칸으로 이동. 자기의 냄새가 있는 방향으로 이동. 특정 우선순위.
-# 여러 마리가 있으면 작은 상어가 차지.
 
-
-
-# %4 ~ %4 +3 까지가 i 번째 상어의 상하좌우 우선순위임.
+# (num-1)%4 ~ (num-1)%4 +3 까지가 num 번째 상어의 상하좌우 우선순위임.
 prioritys = [list(map(int, input().split())) for i in range(M * 4)]
 
 # 북 남 서 동
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
-
 
 def find_all_sharks():
     sharks = []
@@ -67,12 +56,10 @@ def find_direction(shark):
 
     no_scent_direction = []
     my_scent_dirction = []
-    move_direction = []
+
 
 
     for i in range(4):
-
-        direc_flag = False
 
         # priority 에서 꺼내줄 때는 1  빼준 값으로.
         new_direction = priority[i] - 1
@@ -84,26 +71,25 @@ def find_direction(shark):
         if nr < 0 or nr >= N or nc < 0 or nc >= N:
             continue
 
-        if map_[nr][nc][2] != 0 and map_[nr][nc][3] != 0:
-            direc_flag = True
-
-            if map_[nr][nc][2] == num:
+        if map_[nr][nc][2] != 0 and map_[nr][nc][3] > 0:
+            # 자신의 냄새가 있으면.
+            if map_[nr][nc][2] == num and map_[nr][nc][3] > 0:
                 my_scent_dirction.append(new_direction)
+                continue
 
-        if not direc_flag:
+        # 냄새가 없는 것도 우선순위 순으로 추가.
+        else:
             no_scent_direction.append(new_direction)
-
-    # print(shark, no_scent_direction, my_scent_dirction, move_direction)
 
     # 우선 순위로 돌았으므로 0 번째 꺼를 리턴해주면 됨.
     if no_scent_direction:
         return no_scent_direction[0]
 
+    # 자신의 냄새가 있었다면.
     if my_scent_dirction:
         return my_scent_dirction[0]
 
-    if move_direction:
-        return priority[0] - 1
+    return priority[0] - 1
 
 
 # 모든 상어 이동하기.
@@ -112,13 +98,13 @@ def move_all_shark():
 
     global k
 
+    # 상어 중복 체크용 맵
+    dupli_shark_map_ = [[[] for i in range(N)] for r in range(N)]
+
+    # 모든 상어 찾기.
     sharks = find_all_sharks()
 
-
-
-
-
-    # 모든 상어에 대해 수행
+    # 모든 상어에 대해 이동 처리
     for shark in sharks:
 
         num, direc, sent_num, second, r, c = shark
@@ -132,19 +118,21 @@ def move_all_shark():
         if nr < 0 or nr >= N or nc < 0 or nc >= N:
             continue
 
+        # 중복 체크용에 넣어줌.
         dupli_shark_map_[nr][nc].append([num, direc, num, k, r, c])
 
-    # for _ in dupli_shark_map_:
-    #     print(_)
 
     for r in range(N):
         for c in range(N):
 
+
+            # 두 마리 이상의 상어가 들어왔으면 1번째 이후의 것들은 내쫓음.
             if len(dupli_shark_map_[r][c]) >= 2:
 
+                # 첫 상어만 이동 처리.
                 map_[r][c] = dupli_shark_map_[r][c][0][0:4]
 
-                # 1 부터
+                # 그 외 상어는 내 쫓음.
                 for i in range(1, len(dupli_shark_map_[r][c])):
                     num, direc, num, k, pre_r, pre_c = dupli_shark_map_[r][c][i]
 
@@ -155,13 +143,12 @@ def move_all_shark():
             elif len(dupli_shark_map_[r][c]) == 1:
 
                 num, direc, num, k, pre_r, pre_c = dupli_shark_map_[r][c][0]
-                # print(f"???{dupli_shark_map_[r][c][0]}")
 
                 map_[r][c] = [num, direc, num, k]
                 map_[pre_r][pre_c][0] = 0
                 map_[pre_r][pre_c][1] = 0
 
-            # 아무 것도 없었으면 0, 0
+            # # 아무 것도 없었으면 0, 0
             else:
                 map_[r][c][0] = 0
                 map_[r][c][1] = 0
@@ -183,6 +170,7 @@ def move_all_shark():
 
 # 1번 상어만 남아있는 지 여부 체크 함수
 def is_only_first_shark():
+
     global map_
 
     for r in range(N):
@@ -197,22 +185,14 @@ time = 0
 
 while True:
 
-    dupli_shark_map_ = [[[] for i in range(N)] for r in range(N)]
-
-    # print(dupli_shark_map_[2][1])
+    # 1 번 상어만 존재하는 지 여부
     if is_only_first_shark():
         break
 
     time += 1
 
-
-
+    # 모든 상어 움직이기
     move_all_shark()
-
-    # for _ in map_:
-        # print(_)
-
-    # print(f"{time}----------------------------")
 
     if time >= 1000:
         time = -1
