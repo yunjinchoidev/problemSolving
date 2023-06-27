28d6d2c5c71b29221d8af2124f31006a76a35e16
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-06-28 01:27:33 +0900

    크루스알고리즘 re

--- 2023/6월/백준1922.py
+++ 2023/6월/백준1922.py
@@ -1,47 +1,35 @@
 N = int(input())
 M = int(input())
-graph = [[] for i in range(N + 1)]
 
-# for i in range(M):
-
-# graph[s].append((e, d))
-# graph[e].append((s, d))
+edges = []
+for i in range(M):
+    s, e, d = map(int, input().split())
+    edges.append((d, s, e))
 
 parent = [i for i in range(N + 1)]
 
-# print(parent)
-
 
 def find_parent(parent, v):
-
     if parent[v] != v:
         parent[v] = find_parent(parent, parent[v])
 
     return parent[v]
 
 
-def union(a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a <= b:
-        parent[b] = a
+def union(s, e):
+    s = find_parent(parent, s)
+    e = find_parent(parent, e)
+    if s <= e:
+        parent[e] = s
     else:
-        parent[a] = b
+        parent[s] = e
 
 
-edges = []
-
-for i in range(M):
-    s, e, d = map(int, input().split())
-    edges.append((d, s, e))
-
+result = 0
 edges.sort()
-cost = 0
 for edge in edges:
-    d, s, e = edge
-    # print(d,s,e)
-    if find_parent(parent, s) != find_parent(parent, e):  # 사이클이 아닌경우
+    cost, s, e = edge
+    if find_parent(parent, s) != find_parent(parent, e):
         union(s, e)
-        cost += d
-
-print(cost)
+        result += cost
+print(result)
